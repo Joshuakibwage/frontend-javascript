2b45babc0647601cded3e9dc3e973a93f9181aff
Josh <joshuakibwage23@gmail.com>
2025-07-09 13:41:46 +0300

    chore: add TypeScript setup and dependencies

---
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -0,0 +1,46 @@
+
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+
+  mode: "development",
+
+  entry: "./js/main.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devServer: {
+    static: {
+    directory: path.resolve(__dirname, "dist"),
+    },
+    open: true,         // Optional: opens the browser
+    hot: true,          // Optional: enables Hot Module Replacement
+    port: 8080          // Optional: custom port
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development"
+    })
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
