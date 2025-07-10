adf6f72c8a5acbea82d285f9e335aa01f06e5b76
Josh <joshuakibwage23@gmail.com>
2025-07-10 21:38:32 +0300

    feat: add print teacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,10 +35,15 @@ const director1: Directors = {
 console.log(director1);
 
 
-function printTeacher(firstName: string, lastName: string) {
-  return (
-    
-  )
-  printTeacher("John", "Doe")
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
 }
 
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}` 
+}
+
+
+
+console.log(printTeacher("John", "Doe"))
