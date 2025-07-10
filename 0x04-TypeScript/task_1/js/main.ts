301b6df90edab17bfea10c0a8204f85d5b233aed
Josh <joshuakibwage23@gmail.com>
2025-07-10 13:25:02 +0300

    feat: extend teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,7 +4,7 @@ interface Teacher {
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
-    contract: boolean;
+    contract?: boolean;
 }
 
 
@@ -17,3 +17,19 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3)
+
+
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
